109f210c42146540759c8e5d19d13803fd3c22eb
Agyemang166 <gyamfiagyemang999@gmail.com>
2023-09-23 09:48:58 +0000

    second

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,63 +2,21 @@ import {Container, Row, Col} from "react-bootstrap"
 import * as Icon from 'react-bootstrap-icons';
 import Cosmicimage from "../assets/img/cosmicimage.png";
 import Austronaut from "../assets/img/autronaut.png";
-import Fly from "../assets/img/fly.png";
-import { useEffect, useState } from "react";
+
 
 
 export const Banner =() => {
-const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = [ " Web Developer", " Web Designer", " UI/UX Designer" ];
-  const period = 500;
 
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => { clearInterval(ticker) };
-  }, [text])
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
-  }
-
-    
     return(
         <section className="banner" id="home" >
             <Container>
                 <Row className="justify-content-center align-items-center ">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to My portfolio</span>
-                    <h3>"Hi I'm Nana Gyamfi Agyemang"</h3>
-                    <h1> <span className="wrap"></span>A {text}</h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages</p>
-                    <button onClick={() => console.log("connect")}>Let's connect <Icon.ArrowRightCircleFill size={28}/></button>
+                    <h3 className="name">Hi, I'm Nana Gyamfi Agyemang</h3>
+                    <h1 className="d-flex "> <span className="changecontent wrap"></span></h1>
+                    <p>"Hello, I am Agyemang Nana Gyamfi, an industrious scholar of Computer Science at KNUST, with a specialization in React development. My fervor for technology, coupled with my adept problem-solving abilities, distinguishes me in my field. My solid understanding of coding principles and propensity for crafting user-centric applications make me a formidable contender for any technological challenge!"</p>
+                     <a href="#"><button>Let's connect <Icon.ArrowRightCircleFill size={28}/></button></a>
                     </Col>
                     <Col xs={12} md={3} xl={3} >
                       <div className="first">
